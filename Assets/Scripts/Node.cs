5d389ab3902690a338310e651d432e7d9dda0592
Leonhard Pickny <Leonhard.Pickny@gmail.com>
2016-11-20 21:21:53 +0100

    Networking

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,107 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class Node : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-	//reference to gamemanager
-	public int curPlayer;
-
-	public float defaultTimerLength = 1;
-	float timeLimit;
-	bool timerOn;
-
-	public enum NodeState {CLEAN, REINFORCED, INVADED, INFECTED};
-	public enum NodeChangeState {INACTIVE, REINFORCING, INVASION, CLEANING};
-	public NodeState curNodeState;
-	public NodeChangeState curNodeChangeState;
-
-	public GameObject contextMenu;
-
-	public Text debugText;
-
-	void Start () {
-		
-	}
-
-	void startTimer(float howLong){
-		timeLimit = Time.time + howLong;
-		timerOn = true;
-	}
-
-	public void triggerReinforce(){
-		curNodeChangeState = NodeChangeState.REINFORCING;
-		startTimer (defaultTimerLength);
-	}
-
-	public void triggerInvade(){
-		curNodeChangeState = NodeChangeState.INVASION;
-		startTimer (defaultTimerLength);
-	}
-
-	public void triggerClean(){
-		curNodeChangeState = NodeChangeState.CLEANING;
-		startTimer (defaultTimerLength);
-	}
-	
-	void Update () {
-
-		if (timerOn) {
-			if (Time.time >= timeLimit) {
-				
-				timerOn = false;
-				switch (curNodeChangeState) {
-				case NodeChangeState.REINFORCING:
-					doReinforce ();
-					break;
-				case NodeChangeState.INVASION:
-					doInvade ();
-					break;
-				case NodeChangeState.CLEANING:
-					doClean (); 
-					break;
-				}
-			} else {
-				debugText.text = "" + (int)(timeLimit - Time.time);
-			}
-		} 
-	}
-
-	void doReinforce(){
-		curNodeState = NodeState.REINFORCED;
-		debugText.text = "REINFORCED";
-	}
-
-	void doInvade(){
-		//cancel the action of the player
-		curNodeState = NodeState.INVADED;
-		debugText.text = "INVADED";
-	}
-
-	void doClean(){
-		curNodeState = NodeState.CLEAN;
-		debugText.text = "CLEAN";
-	}
-
-	void infectNode(){
-		cancelAction ();
-		curNodeState = NodeState.INFECTED;
-	}
-
-	void cancelAction(){
-		timerOn = false;
-		curNodeChangeState = NodeChangeState.INACTIVE;
-	}
-
-
-	public void displayContextMenu (){
-		if (contextMenu.activeSelf)
-			contextMenu.SetActive (false);
-		else
-			contextMenu.SetActive (true);
-		//enable or disable things based on what is available
-	}
-		
-
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(0, y, 0);
+        transform.Translate(x, 0, 0);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+        transform.position = new Vector4(-14, -5, -5);
+    }
+}
