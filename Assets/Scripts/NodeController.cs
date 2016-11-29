c17ae0d285a4b36acab781b9b6cb756dae48ceb4
Leonhard Pickny <Leonhard.Pickny@gmail.com>
2016-11-29 20:21:09 +0100

    Nodes

---
+++ Assets/Scripts/NodeController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeController : MonoBehaviour
+{
+    private SpriteRenderer stateColorRenderer;   //Renders the outer color of the node.
+    private SpriteRenderer ownerColorRenderer;   //Renders the inner color of the node.
+    private SpriteRenderer symbolRenderer;  //Renders the symbol of the node.
+
+    private Color ownerColor;
+    private Color stateColor;
+
+    enum OwnerColor // your custom enumeration
+    {
+        Green,
+        Purple,
+        Orange,
+        Blue
+    };
+
+    enum StateColor // your custom enumeration
+    {
+        White,
+        Green,
+        Red
+    };
+
+    public OwnerColor OwnerColor;
+    public string State;
+
+    public bool isInfected;
+    public bool isProtected;
+
+    public int timeTilProtected;
+    public int timeTilHealed;
+
+    void Awake()
+    {
+        switch ()
+        ownerColor = 
+
+        //Get all the sprite renderers of the node.
+        foreach (Transform t in transform)
+        {
+            if (t.name == "Outer Color")
+            {
+                ownerColorRenderer = t.GetComponent<SpriteRenderer>();
+                ownerColorRenderer.color = ownerColor;
+            }
+            else if (t.name == "Inner Color")
+            {
+                stateColorRenderer = t.GetComponent<SpriteRenderer>();
+                stateColorRenderer.color = stateColor;
+            }
+            else if (t.name == "Symbol")
+            {
+                symbolRenderer = t.GetComponent<SpriteRenderer>();
+            }
+        }
+    }
+
+    void StartInfection()
+    {
+        if (!isProtected && !isInfected)
+        {
+            StartCoroutine(Infect());
+        }  
+    }
+
+    IEnumerator Infect()
+    {
+        yield return new WaitForSeconds(40);
+        isInfected = true;
+    }
+
+    void StartProtecting()
+    {
+        StartCoroutine(Protect());
+    }
+
+    IEnumerator Protect()
+    {
+        yield return new WaitForSeconds(timeTilProtected);
+        isProtected = true;
+    }
+
+    void StartHealing()
+    {
+        if (isInfected)
+        {
+            StartCoroutine(Heal());
+        }
+    }
+
+    IEnumerator Heal()
+    {
+        yield return new WaitForSeconds(timeTilHealed);
+        isInfected = false;
+    }
+}
