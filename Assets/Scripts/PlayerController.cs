4d9a0b93f117fbd3b1501d50350252d1c0a1a8ee
Leonhard Pickny <Leonhard.Pickny@gmail.com>
2016-12-16 10:59:23 +0100

    Changed PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,84 +1,92 @@
 ﻿using UnityEngine;
-using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour
+public class PlayerController : MonoBehaviour
 {
-	GameManager gameManager;
-	[SyncVar] public Color playerColor;
+    public Color playerColor;
 
-    void Update()
-    {
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-    }
 
-    void OnStartLocalPlayer()
-    {
-		AssignColor ();
-    }
-
-	void AssignColor()
-	{
-        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
-
-        bool assignedColor = false;
-
-		while (!assignedColor) 
-		{
-			int rnd = (int)Random.Range (1, 4);
-
-			switch (rnd) {
-			case 1:
-				{
-					if (!gameManager.colorIsTakenA)
-					{
-						playerColor = gameManager.playerColorA;
-						gameManager.colorIsTakenA = true;
-						assignedColor = true;
-					}
-					return;
-				}
-			case 2:
-				{
-					if (!gameManager.colorIsTakenB)
-					{
-						playerColor = gameManager.playerColorB;
-						gameManager.colorIsTakenB = true;
-						assignedColor = true;
-					}
-					return;
-				}
-			case 3:
-				{
-					if (!gameManager.colorIsTakenC)
-					{
-						playerColor = gameManager.playerColorC;
-						gameManager.colorIsTakenC = true;
-						assignedColor = true;
-					}
-					return;
-				}
-			case 4:
-				{
-					if (!gameManager.colorIsTakenD)
-					{
-						playerColor = gameManager.playerColorD;
-						gameManager.colorIsTakenD = true;
-						assignedColor = true;
-					}
-					return;
-				}
-			}
-		}
-
-        CmdAssignedColor(playerColor);
-    }
-
-    [Command]
-    public void CmdAssignedColor(Color c)
-    {
-        playerColor = c;
-    }
 }
+
+//using UnityEngine.Networking;
+
+//public class PlayerController : NetworkBehaviour
+//{
+//	GameManager gameManager;
+//	[SyncVar] public Color playerColor;
+
+//    void Update()
+//    {
+//        if (!isLocalPlayer)
+//        {
+//            return;
+//        }
+//    }
+
+//    void OnStartLocalPlayer()
+//    {
+//		AssignColor ();
+//    }
+
+//	void AssignColor()
+//	{
+//        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+//        bool assignedColor = false;
+
+//		while (!assignedColor) 
+//		{
+//			int rnd = (int)Random.Range (1, 4);
+
+//			switch (rnd) {
+//			case 1:
+//				{
+//					if (!gameManager.colorIsTakenA)
+//					{
+//						playerColor = gameManager.playerColorA;
+//						gameManager.colorIsTakenA = true;
+//						assignedColor = true;
+//					}
+//					return;
+//				}
+//			case 2:
+//				{
+//					if (!gameManager.colorIsTakenB)
+//					{
+//						playerColor = gameManager.playerColorB;
+//						gameManager.colorIsTakenB = true;
+//						assignedColor = true;
+//					}
+//					return;
+//				}
+//			case 3:
+//				{
+//					if (!gameManager.colorIsTakenC)
+//					{
+//						playerColor = gameManager.playerColorC;
+//						gameManager.colorIsTakenC = true;
+//						assignedColor = true;
+//					}
+//					return;
+//				}
+//			case 4:
+//				{
+//					if (!gameManager.colorIsTakenD)
+//					{
+//						playerColor = gameManager.playerColorD;
+//						gameManager.colorIsTakenD = true;
+//						assignedColor = true;
+//					}
+//					return;
+//				}
+//			}
+//		}
+
+//        CmdAssignedColor(playerColor);
+//    }
+
+//    [Command]
+//    public void CmdAssignedColor(Color c)
+//    {
+//        playerColor = c;
+//    }
+//}
